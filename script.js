b4d305625d0dbb8e6bed9a8ceb63b7b12da4579e
TheBims7 <farizbimaprayoga@gmail.com>
2026-05-03 22:40:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,30 @@ ul.addEventListener("click", function () {
     }
 });
 
+const sections = document.querySelectorAll("section, .service");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
 const words = ["Fariz Bima Prayoga "];
 let i = 0;
 let j = 0;
