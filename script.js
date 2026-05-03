49b2dcd2827217fd260391fd1ff197c3ab443f0c
TheBims7 <farizbimaprayoga@gmail.com>
2026-05-03 19:32:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const header = document.querySelector("header");
+const checkbox = document.getElementById("nav-button");
+const ul = document.querySelector("ul");
+
+window.addEventListener("scroll", function () {
+    if (!checkbox.checked) {
+        if (window.scrollY > 50) {
+            header.classList.add("active");
+        } else {
+            header.classList.remove("active");
+        }
+    }
+});
+
+checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+        header.classList.remove("active");
+    } else {
+        // balik ke kondisi scroll
+        if (window.scrollY > 50) {
+            header.classList.add("active");
+        } else {
+            header.classList.remove("active");
+        }
+    }
+});
+
+ul.addEventListener("click", function () {
+    checkbox.checked = false;
+
+    // balik ke kondisi scroll
+    if (window.scrollY > 50) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
+const words = ["Fariz Bima Prayoga "];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+    const target = document.getElementById("typing");
+
+    if (i < words.length) {
+        currentWord = words[i];
+        if (!isDeleting && j <= currentWord.length) {
+            target.textContent = currentWord.substring(0, j);
+            j++;
+        } else if (isDeleting && j >= 0) {
+            target.textContent = currentWord.substring(0, j);
+            j--;
+        }
+
+        if (j === currentWord.length) {
+            isDeleting = true;
+            setTimeout(type, 2000); // Pause after typing
+            return;
+        }
+
+        if (isDeleting && j === 0) {
+            isDeleting = false;
+            i = (i + 1) % words.length;
+        }
+    }
+
+    setTimeout(type, isDeleting ? 200 : 200);
+}
+
+
+document.addEventListener("DOMContentLoaded", type);
